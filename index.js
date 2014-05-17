4904e5e43c42d7d7021a458d5987f37b3f0833d6
Chris Olsen <olsen.chris@gmail.com>
2014-05-16 21:08:41 -0600

    init

---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var dom = require('dom');
+
+var TRANSITION_TIME = 500;
+var ABS_STYLE = { position: 'absolute', top: 0, left: 0, opacity: 0};
+
+
+/**
+ * Loads the element into the DOM
+ *
+ * @param {Element} element - html element to load
+ */
+module.exports = function(parentElem, selector, elem, options) {
+
+  if (arguments.length <= 3) {
+    options     = elem;
+    elem        = selector;
+    selector    = parentElem;
+    parentElem  = dom('body')[0];
+  }
+
+  var $insertElem = dom(parentElem).find(selector);
+  var $el         = dom(elem);
+
+  // prevent re-injection of the same layout
+  if ($insertElem.html() === $el.html) return;
+
+  // remove previous element
+  if ($insertElem[0].hasChildNodes()) {
+    var $elemToRemove = dom($insertElem[0].firstChild);
+    $elemToRemove
+      .removeClass('fade-in')
+      .addClass('fade-out');
+
+    setTimeout(function() {
+      $elemToRemove.remove();
+    }, TRANSITION_TIME);
+  }
+
+  $el
+    .css(ABS_STYLE)
+    .css('z-index', 2)
+    .addClass('animated')
+    .addClass('fade-in');
+
+  $el.appendTo($insertElem);
+
+  // once faded in, decrement the z-index
+  setTimeout(function() {
+    $el.css('z-index', 1);
+  }, TRANSITION_TIME);
+
+  return elem;
+};
+
